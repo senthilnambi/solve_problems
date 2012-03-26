e07490a52cc9ce9488ad1973053214bc835d630b
Senthil A <senthil196@gmail.com>
2012-03-26 03:26:16 -0700

    added Rails Info & specs

---
+++ ruby_patterns/rails/info.rb
@@ -0,0 +1,63 @@
+# Originally from 'railties/lib/rails/info.rb'
+#
+# Changes:
+#   * Removed `require cgi`.
+#   * Added `require 'activesupport/all'`
+#   * Removed most methods to make it easier to understand.
+#   * Added `extend self`, which removes need for `class << self` to define `property`
+#
+require 'active_support/core_ext/module/attribute_accessors'
+
+module Info
+  mattr_accessor :properties
+  class << (@@properties = [])
+    def names
+      map {|val| val.first }
+    end
+  end
+
+  extend self
+
+  def property(name, value = nil)
+    value ||= yield
+    properties << [name, value] if value
+  rescue Exception
+  end
+
+  property 'Ruby version', RUBY_VERSION
+
+  property 'RubyGems version' do
+    Gem::RubyGemsVersion
+  end
+
+  # BUG: does not rescue error if NonExistent is passed as argument
+  property 'NonExistent' do
+    NonExistent
+  end
+end
+
+describe Info do
+  subject { described_class }
+
+  def find_property(name)
+    subject.properties.find {|x| x[0] == name}
+  end
+
+  context 'when property is called' do
+    it '(with String) returns Ruby version' do
+      find_property('Ruby version')[1].should == '1.9.3'
+    end
+
+    it '(with block) returns RubyGems version' do
+      find_property('RubyGems version')[1].should == '1.8.16'
+    end
+
+    it 'rescues non-existent property errors' do
+      find_property('NonExistent').should == nil
+    end
+  end
+
+  it 'defines methods by opening singleton class on a variable' do
+    subject.properties.names.count.should == 2
+  end
+end
